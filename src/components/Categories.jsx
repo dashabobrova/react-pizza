2c4f0b3b966a823bf5758656f1b196b620bf4772
dashabobrova <work.dbobrova.dasha@gmail.com>
2021-10-24 15:50:13 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export const Categories = ({ items }) => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeItem === null ? "active" : ""}
+          onClick={() => onSelectItem(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? "active" : ""}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Cart = () => {
+    return (
+        <div>
+            <h1>Корзинка</h1>
+        </div>
+    )
+}
